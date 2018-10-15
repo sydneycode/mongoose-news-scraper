1007f337f394e607421b7fd656c726b09bf0e383
sydneycode <sstaggs12@gmail.com>
2018-10-15 02:10:05 -0700

    updated MONGODB_URI after adding mLab

--- server.js
+++ server.js
@@ -37,9 +37,10 @@ app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB (connect to remote mongolab database if deployed; 
 // otherwise connect to the local mongoHeadlines database)
-//var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-//mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+//mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
 
 // Routes
 
